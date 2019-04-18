e964acbd4170607a5ab78f1799f6b230e0f0095d
fmuniz351987 <muniz.r.felipe@gmail.com>
2019-04-18 08:39:34 -0300

    [WIP] pequenas mudanças no estudante

--- lista_estudantes.c
+++ lista_estudantes.c
@@ -14,23 +14,44 @@ typedef struct ListaEstudantes {
     int length;
 } ListaEstudantes;
 
-ListaEstudantes* nova_lista() {
-    ListaEstudantes *lista;
-
-    lista = (ListaEstudantes*) malloc(sizeof(ListaEstudantes));
-    lista->head = NULL;
-    lista->last = NULL;
-    lista->length = 0;
-    return lista;
-}
-
-Estudante* novo_estudante(char* nome, float nota) {
+Estudante* novo_estudante(char* nome, float nota, Estudante* proximo, Estudante *anterior) {
     Estudante *estudante;
 
     estudante = (Estudante*) malloc(sizeof(Estudante));
     estudante->nome = nome;
     estudante->nota = nota;
+    estudante->proximo = proximo;
+    estudante->anterior = anterior;
     return estudante;
+}
+
+ListaEstudantes* nova_lista() {
+    Estudante *head;
+    ListaEstudantes *lista;
+
+    lista = (ListaEstudantes*) malloc(sizeof(ListaEstudantes));
+    head = novo_estudante(NULL, 0, NULL, NULL);
+    lista->head = head;
+    lista->last = head;
+    lista->length = 0;
+    return lista;
+}
+
+void inserir_estudante(ListaEstudantes *lista, char *nome, float nota) {
+    Estudante *estudante;
+
+    estudante = novo_estudante(nome, nota);
+    estudante->anterior = lista->last;
+    lista->last->proximo = estudante;
+    lista->length ++;
+}
+
+void imprimir_lista_de_nomes(ListaEstudantes *lista) {
+    Estudante* proximo = lista->head;
+
+    for(int i = 0; proximo->proximo != NULL; i++) {
+        
+    }
 }
 
 void deletar_estudante(Estudante *estudante) {
@@ -42,9 +63,7 @@ int main() {
     Estudante *estudante;
 
     lista = nova_lista();
-    printf("%p %p %i \n", lista->head, lista->last, lista->length);
-    estudante = novo_estudante("Felipe", 4.2);
-    printf("%s %.2f\n", estudante->nome, estudante->nota);
+    estudante = novo_estudante("Felipe", 4.2, NULL, NULL);
     deletar_estudante(estudante);
     return 0;
 }
